99898b3b2e5af1bd16ae8dfb528d1511b5e5510f
akihyro <akihyro@users.noreply.github.com>
2014-07-15 18:05:30 +0900

    アプリケーションクラスを実装

---
+++ src/main/java/akihyro/cloudprintconsole/CloudPrintConsoleApplication.java
@@ -0,0 +1,18 @@
+package akihyro.cloudprintconsole;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * アプリケーション。
+ */
+@ApplicationPath("/")
+public class CloudPrintConsoleApplication extends Application {
+
+    /**
+     * コンストラクタ。
+     */
+    public CloudPrintConsoleApplication() {
+    }
+
+}
